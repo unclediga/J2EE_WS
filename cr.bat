9cf5457fb93811dc984596c297c868e65224ef88
unclediga <zowie_i@mail.ru>
2022-06-08 12:28:59 +0300

    create ch06

--- cr.bat
+++ cr.bat
@@ -1,1 +1,1 @@
-mvn archetype:generate -B -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-webapp -DarchetypeVersion=1.4 -DgroupId=ru.unclediga.example.ws -DartifactId=restful_ch05 -Dversion=1.0 -Dpackage=ru.unclediga.book.restful.ch05 
+mvn archetype:generate -B -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-webapp -DarchetypeVersion=1.4 -DgroupId=ru.unclediga.example.ws -DartifactId=restful_ch06 -Dversion=1.0 -Dpackage=ru.unclediga.book.restful.ch06 
