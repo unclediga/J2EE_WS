8fd22dcfa83844dc4ce18efc49a945df83ed2a3e
unclediga <zowie_i@mail.ru>
2022-06-17 14:21:39 +0300

    archetype jersey-quickstart-webapp

---
+++ cr_jersey.bat
@@ -0,0 +1,8 @@
+mvn archetype:generate -B^
+   -DarchetypeGroupId=org.glassfish.jersey.archetypes^
+   -DarchetypeArtifactId=jersey-quickstart-webapp^
+   -DarchetypeVersion=2.22.2^
+   -DgroupId=ru.unclediga.example.ws^
+   -DartifactId=javabrains^
+   -Dversion=1.0^
+   -Dpackage=ru.unclediga.javabrains.jaxrs
