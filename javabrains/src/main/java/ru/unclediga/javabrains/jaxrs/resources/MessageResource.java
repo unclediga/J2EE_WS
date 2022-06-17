b8cc40255f6d5a7d3b6ad3d0bec5d6c48581e7f3
unclediga <zowie_i@mail.ru>
2022-06-17 18:10:48 +0300

    17.Accessing Path params

--- javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/resources/MessageResource.java
+++ javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/resources/MessageResource.java
@@ -5,6 +5,7 @@ import ru.unclediga.javabrains.jaxrs.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -17,5 +18,12 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_XML)
     public List<Message> getMessages(){
         return service.getAllMessages();
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") long id){
+        return service.getMessage(id);
     }
 }
