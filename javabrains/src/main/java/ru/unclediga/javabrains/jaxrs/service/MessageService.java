c484a4ca60fe668e822ce9dfcd725cd19082ccc8
unclediga <zowie_i@mail.ru>
2022-06-17 18:10:35 +0300

    16.Building Service Stubs

--- javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/service/MessageService.java
+++ javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/service/MessageService.java
@@ -1,16 +1,40 @@
 package ru.unclediga.javabrains.jaxrs.service;
 
+import ru.unclediga.javabrains.jaxrs.data.DatabaseClass;
 import ru.unclediga.javabrains.jaxrs.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MessageService {
 
+    private final Map<Long, Message> messages = DatabaseClass.getMessages();
+
+    public MessageService() {
+        messages.put(1L, new Message(1L, "Hello, World!", "unclediga"));
+        messages.put(2L, new Message(2L, "Hello, Jersey", "unclediga"));
+    }
+
     public List<Message> getAllMessages() {
-        List<Message> messages = new ArrayList<>();
-        messages.add(new Message(1L, "Hello, World!", "unclediga"));
-        messages.add(new Message(2L, "Hello, Jersey", "unclediga"));
-        return messages;
+        return new ArrayList<>(messages.values());
+    }
+
+    public Message getMessage(long id){
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message){
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message){
+        return messages.putIfAbsent(message.getId(), message);
+    }
+
+    public Message removeMessage(long id){
+        return messages.remove(id);
     }
 }
