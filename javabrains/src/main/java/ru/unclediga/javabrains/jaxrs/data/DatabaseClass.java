310efe98f7ba145b2154918f4a62b9d05e4d19ee
unclediga <zowie_i@mail.ru>
2022-06-21 19:59:18 +0300

    java.lang.ClassNotFoundException: org.glassfish.jersey.server.internal.RuntimeExecutorsBinder$BackgroundSchedulerLiteral

--- javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/data/DatabaseClass.java
+++ javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/data/DatabaseClass.java
@@ -10,6 +10,9 @@ public class DatabaseClass {
     private static Map<Long, Message> messages = new HashMap<>();
     private static Map<Long, Profile> profiles = new HashMap<>();
 
+    public DatabaseClass() {
+    }
+
     public static Map<Long, Message> getMessages() {
         return messages;
     }
