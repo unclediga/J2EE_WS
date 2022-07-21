9a5e4772aa1fae6058f26683e8281db4bc51fe8c
unclediga <zowie_i@mail.ru>
2022-07-21 14:35:37 +0300

    31.Content Negotiation (II)

--- javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/resources/ProfileResource.java
+++ javabrains/src/main/java/ru/unclediga/javabrains/jaxrs/resources/ProfileResource.java
@@ -11,30 +11,19 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/profiles")
+@Produces(value = {MediaType.APPLICATION_XML, MediaType.TEXT_XML, MediaType.APPLICATION_JSON})
 public class ProfileResource {
     private final ProfileService service = new ProfileService();
 
     @GET
-    @Produces(value = {MediaType.APPLICATION_XML, MediaType.TEXT_XML})
     public List<Profile> getProfiles() {
         return service.getAllProfiles();
     }
+
     @GET
     @Path("/{pname}")
-    @Produces(value={MediaType.APPLICATION_XML,MediaType.TEXT_XML})
-    public Profile getProfile(@PathParam("pname") String profileName){
+    public Profile getProfile(@PathParam("pname") String profileName) {
         return service.getProfile(profileName);
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Profile> getProfilesJ(){
-        return service.getAllProfiles();
-    }
-    @GET
-    @Path("/{pname}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Profile getProfileJ(@PathParam("pname") String profileName){
-        return service.getProfile(profileName);
-    }
 }
